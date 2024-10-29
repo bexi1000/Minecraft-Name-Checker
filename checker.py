19c9c32ba366c277525a4a8002d3bcf44cba1823
bexi <139641517+bexi1000@users.noreply.github.com>
2024-10-29 20:35:02 +0300

    Add files via upload

---
+++ checker.py
@@ -0,0 +1,18 @@
+import random
+import requests
+
+harfler = "abcdefghjklmnoprstvyz1234567890"
+
+base_url = "https://api.mojang.com/users/profiles/minecraft/"
+
+for _ in range(200):
+    username = "".join(random.sample(harfler, 3))
+    get_url = f"{base_url}{username}"
+    response = requests.get(get_url)
+
+    if response.status_code == 200:
+        print(f"Kullanıcı Adı: {username} (Mevcut)")
+    elif response.status_code == 204:
+        print(f"Kullanıcı Adı: {username} (Kullanılabilir)")
+    else:
+        print(f"Kullanıcı Adı: {username} (Bilinmeyen durum veya hata)")
